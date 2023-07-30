a2f8637e6a5e74ccf8147e35e3e792c7a59d66e0
Kostiantyn Matiushenko <87534952+jojonoskill@users.noreply.github.com>
2023-07-30 23:16:27 +0300

    Update index.js

--- link-tree-client/src/index.js
+++ link-tree-client/src/index.js
@@ -9,14 +9,7 @@ import { initializeApp } from "firebase/app";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBJ-LR4aJNWkR6dpcGmYcCvpoUAkHwz_Ts",
-  authDomain: "link-tree-clone-f58ef.firebaseapp.com",
-  databaseURL: "https://link-tree-clone-f58ef-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "link-tree-clone-f58ef",
-  storageBucket: "link-tree-clone-f58ef.appspot.com",
-  messagingSenderId: "879414992618",
-  appId: "1:879414992618:web:32e432ddf8b96a5a0a8193",
-  measurementId: "G-W24RGNQV7P"
+  //PASTE YOUR FIREBASECONFIG HERE
 };
 
 const app = initializeApp(firebaseConfig);
